d032687476c533965d070b3cb4e56339e9fc827e
Sankar Rockzz <Sankar.behera66@gmail.com>
2016-04-12 20:36:08 +0530

    Update main cpp
    program is added.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
 
+#include<stdio.h>
+ main()
+{
+ int c=6;
+ int *b; b=&c; /// variables use in the demonstration;
+ int *a=(int*)new int; /// 'a' is a pointer;
+ int **a=(int**)new int;/// 'a' is a double pointer;
+ a=&b;
+ printf("%d %d",**a,&b); /// value at c= **a
+ return 0;
+}
