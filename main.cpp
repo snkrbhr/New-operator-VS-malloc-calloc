1c354fb2d738a54bb3bf43f9fde89d9d5fc96400
Sankarrockzz <Sankar.behera66@gmail.com>
2016-04-12 21:20:33 +0530

    Main.cpp is modifided.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
 
-#include<stdio.h>
- main()
+main()
 {
- int c=6;
- int *b; b=&c; /// variables use in the demonstration;
- int *a=(int*)new int; /// 'a' is a pointer;
- int **a=(int**)new int;/// 'a' is a double pointer;
- a=&b;
- printf("%d %d",**a,&b); /// value at c= **a
+
+    cout<<"Using MALLOC Function : \n";
+ /******************* MALLOC Function *************************/
+    //creating an array dynamically of size 5 using malloc;
+    int *mPtr = (int*) malloc(sizeof(int) * 5);
+    for(int i=0;i<5;i++)
+       {
+         *(mPtr+i)=i+1;     //input
+         cout<<*(mPtr+i)<<" ";   //Output
+       }
+
+   cout<<endl<<endl;
+   cout<<"Using NEW Operator : \n";
+ /******************* NEW Operator *************************/
+
+
+ int *nPtr= new int [5];
+ for(int i=0;i<5;i++)
+      {
+        *(nPtr+i)=i+1;     //input
+        cout<<*(nPtr+i)<<" ";   //Output
+      }
+
  return 0;
 }
